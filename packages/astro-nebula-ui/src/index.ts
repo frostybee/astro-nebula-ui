081d3e58493c5e8b9df5a02d480db203dbacb788
frostybee <rsleiman2@gmail.com>
2025-08-22 02:30:41 -0400

    Refactor the aside components

--- packages/astro-nebula-ui/src/index.ts
+++ packages/astro-nebula-ui/src/index.ts
@@ -1,7 +1,7 @@
 // Re-export all components for easy importing
 export { default as Callout } from './components/callouts/Callout.astro';
 export { default as TextScaler } from './components/ui/TextScaler.astro';
-export { default as NewCallout } from './components/callouts/NewCallout.astro';
+export { default as NeoCallout } from './components/callouts/NeoCallout.astro';
 export { default as CollapsiblePanel } from './components/CollapsiblePanel.astro';
 export { default as SlideViewer } from './components/slideViewer/SlideViewer.astro';
 export { default as LearningObjectives } from './components/LearningObjectives.astro';
